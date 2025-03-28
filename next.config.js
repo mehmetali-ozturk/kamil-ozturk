2d22f3918ab8f86f60adedfb3e023d284c97532b
Mehmetali ÖZTÜRK <mehmetali.ozturk@hotmail.com>
2025-03-28 23:32:23 +0300

    fix4

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     poweredByHeader: false,
-    output: 'standalone',
-    // This will ensure more consistent behavior between development and production
-    webpack: (config) => {
-      // Only modify what we need to fix the case sensitivity issues
-      config.resolve.symlinks = false;
-      
-      return config;
-    }
   }
   
   module.exports = nextConfig;
