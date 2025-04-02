f3bb8287c6140a1a4b42498c8af15fd9c51b3975
Mehmetali ÖZTÜRK <mehmetali.ozturk@hotmail.com>
2025-04-02 18:14:40 +0300

    Başlık fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Kamil OztUrk",
-  description: ".",
+  title: "Kamil Öztürk",
+  description: "Elektrik Şefi",
 };
 
 export default function RootLayout({
